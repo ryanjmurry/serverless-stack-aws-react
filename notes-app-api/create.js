6e57efc4614d590f1c40f6b3a4756b3473d2074a
Ryan Murry <ryanjmurry@gmail.com>
2018-12-15 13:11:27 -0800

    update dynamodb region

--- notes-app-api/create.js
+++ notes-app-api/create.js
@@ -1,6 +1,8 @@
 import uuid from 'uuid';
 import AWS from 'aws-sdk';
 
+AWS.config.update({ region: 'us-east-2' });
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export function main(event, context, callback) {
