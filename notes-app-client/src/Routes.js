5ac087a413479eb96069302149262562718e3166
Ryan Murry <ryanjmurry@gmail.com>
2018-12-18 23:01:02 -0800

    add signup route

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -1,15 +1,16 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import NotFound from './containers/NotFound';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 import AppliedRoute from './components/AppliedRoutes';
 
-export default ({ childProps }) =>
+export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    { /* Finally, catch all unmatched routes */ }
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <Route component={NotFound} />
-  </Switch>;
-
+  </Switch>
+);
