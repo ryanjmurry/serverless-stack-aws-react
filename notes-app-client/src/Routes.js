d2b776a3aa3888c3caa116504c03166216198fa3
Ryan Murry <ryanjmurry@gmail.com>
2018-12-18 23:15:13 -0800

    add new note route

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -4,6 +4,7 @@ import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
+import NewNote from "./containers/NewNote";
 import AppliedRoute from './components/AppliedRoutes';
 
 export default ({ childProps }) => (
@@ -11,6 +12,7 @@ export default ({ childProps }) => (
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <Route component={NotFound} />
   </Switch>
 );
