78785bc9fb2fe5df399226d9b0b7c1f57a9b42cf
Ryan Murry <ryanjmurry@gmail.com>
2018-12-19 20:07:55 -0800

    render list of user notes on home page, add styles

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
-import { PageHeader, ListGroup } from "react-bootstrap";
-import "./Home.css";
+import React, { Component } from 'react';
+import { API } from 'aws-amplify';
+import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './Home.css';
 
 export default class Home extends Component {
   constructor(props) {
@@ -12,8 +14,43 @@ export default class Home extends Component {
     };
   }
 
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await this.notes();
+      this.setState({ notes });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  notes() {
+    return API.get('notes', '/notes');
+  }
+
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map((note, i) => // if index is not 0, render note item, if 0, render create new note
+      i !== 0 ? (
+        <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+          <ListGroupItem header={note.content.trim().split('\n')[0]}>
+            {'Created: ' + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+        </LinkContainer>
+      ) : (
+        <LinkContainer key="new" to="/notes/new">
+          <ListGroupItem>
+            <h4>
+              <b>{'\uFF0B'}</b> Create a new note
+            </h4>
+          </ListGroupItem>
+        </LinkContainer>
+      )
+    );
   }
 
   renderLander() {
@@ -29,9 +66,7 @@ export default class Home extends Component {
     return (
       <div className="notes">
         <PageHeader>Your Notes</PageHeader>
-        <ListGroup>
-          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
-        </ListGroup>
+        <ListGroup>{!this.state.isLoading && this.renderNotesList(this.state.notes)}</ListGroup>
       </div>
     );
   }
