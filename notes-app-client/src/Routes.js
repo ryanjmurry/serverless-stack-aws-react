e12becaaf76a2c9b332b99103e792550b7d9c7bd
Ryan Murry <ryanjmurry@gmail.com>
2018-12-19 20:37:40 -0800

    update applied routes to auth or unauth routes

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -4,17 +4,19 @@ import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
-import NewNote from "./containers/NewNote";
-import Notes from "./containers/Notes";
+import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
 import AppliedRoute from './components/AppliedRoutes';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
-    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <Route component={NotFound} />
   </Switch>
 );
