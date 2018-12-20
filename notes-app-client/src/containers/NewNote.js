e59fc0648d6a61e20c5e50701b34ff775501fa18
Ryan Murry <ryanjmurry@gmail.com>
2018-12-19 17:19:47 -0800

    add s3upload helper and create note with attachment

--- notes-app-client/src/containers/NewNote.js
+++ notes-app-client/src/containers/NewNote.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
-import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import LoaderButton from "../components/LoaderButton";
-import config from "../config";
-import "./NewNote.css";
+import React, { Component } from 'react';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import LoaderButton from '../components/LoaderButton';
+import config from '../config';
+import { API } from 'aws-amplify';
+import { s3Upload } from '../libs/awsLib';
+import './NewNote.css';
 
 export default class NewNote extends Component {
   constructor(props) {
@@ -12,7 +14,7 @@ export default class NewNote extends Component {
 
     this.state = {
       isLoading: null,
-      content: ""
+      content: ''
     };
   }
 
@@ -24,21 +26,40 @@ export default class NewNote extends Component {
     this.setState({
       [event.target.id]: event.target.value
     });
-  }
+  };
 
   handleFileChange = event => {
     this.file = event.target.files[0];
-  }
+  };
 
   handleSubmit = async event => {
     event.preventDefault();
 
     if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
-      alert(`Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE/1000000} MB.`);
+      alert(`Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE / 1000000} MB.`);
       return;
     }
 
     this.setState({ isLoading: true });
+
+    try {
+      const attachment = this.file ? await s3Upload(this.file) : null;
+
+      await this.createNote({
+        attachment,
+        content: this.state.content
+      });
+      this.props.history.push('/');
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  };
+
+  createNote(note) {
+    return API.post('notes', '/notes', {
+      body: note
+    });
   }
 
   render() {
