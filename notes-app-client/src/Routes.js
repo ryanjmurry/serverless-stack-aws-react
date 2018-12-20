1e7bde65bb3313799b921475840b8ac97aa87b17
Ryan Murry <ryanjmurry@gmail.com>
2018-12-19 20:07:25 -0800

    add route to view and edit note

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -5,6 +5,7 @@ import NotFound from './containers/NotFound';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import AppliedRoute from './components/AppliedRoutes';
 
 export default ({ childProps }) => (
@@ -13,6 +14,7 @@ export default ({ childProps }) => (
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <Route component={NotFound} />
   </Switch>
 );
