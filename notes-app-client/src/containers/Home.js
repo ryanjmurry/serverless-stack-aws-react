3d6f518c0543d1031458d5bf928edcc4ec1f4dbb
Ryan Murry <ryanjmurry@gmail.com>
2018-12-19 17:21:10 -0800

    conditionally render home page to show notes if user is authenticated

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
+import { PageHeader, ListGroup } from "react-bootstrap";
 import "./Home.css";
 
-class Home extends Component {
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      notes: []
+    };
+  }
+
+  renderNotesList(notes) {
+    return null;
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
-        <div className="lander">
-          <h1>Scratch</h1>
-          <p>A simple note taking app</p>
-        </div>
+        {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
       </div>
     );
   }
 }
-
-export default Home;
